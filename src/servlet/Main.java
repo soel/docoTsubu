c7e92704eaf3c6dc1a9e21f65f8f883b5b690fc7
kanbara <akihiro.kanbara@l-is-b.com>
2015-09-18 17:58:37 +0900

    エラーメッセージを表示する機能の追加

--- src/servlet/Main.java
+++ src/servlet/Main.java
@@ -79,6 +79,9 @@ public class Main extends HttpServlet {
 			
 			// アプリケーションスコープにつぶやきリストを保存
 			application.setAttribute("mutterList", mutterList);
+		} else {
+			// エラーメッセージをリクエストスコープに保存
+			request.setAttribute("errorMsg", "つぶやきが入力されていません");
 		}
 		
 		// メイン画面にフォワード
